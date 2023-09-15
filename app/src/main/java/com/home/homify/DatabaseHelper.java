cbe776449f840fb2f3067a40e5eaf870019a69cb
Axi20 <varagn19@gmail.com>
2023-09-15 22:17:50 +0200

    Database operations - create recipes table
    Add functions to handle the modifications in the recipes table

--- app/src/main/java/com/home/homify/DatabaseHelper.java
+++ app/src/main/java/com/home/homify/DatabaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.sql.Blob;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "home_inventory.db";
     private static final int DATABASE_VERSION = 1;
@@ -12,13 +14,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_HOUSEHOLD = "household";
     private static final String TABLE_CLEANING = "cleaning";
     private static final String TABLE_HYGIENE = "hygiene";
+    public static final String TABLE_RECIPES = "recipes";
 
     // Common column names.
     private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_ITEM_NAME = "_item_name";
-    private static final String COLUMN_CATEGORY = "_category";
+    public static final String COLUMN_ITEM_NAME = "_item_name";
+    public static final String COLUMN_CATEGORY = "_category";
     private static final String COLUMN_QUANTITY = "_quantity";
     private static final String COLUMN_UNIT = "_unit";
+
+    // Recipes table column names
+    private static final String COLUMN_COOKING_TIME = "cooking_time";
+    private static final String COLUMN_SERVINGS = "servings";
+    private static final String COLUMN_INGREDIENTS = "ingredients";
+    private static final String COLUMN_METHOD = "method";
+    public static final String COLUMN_IMAGE_PATH = "image_path";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -34,12 +44,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(createTableQuery);
     }
 
+    public void createRecipesTable(SQLiteDatabase db) {
+        String createTableQuery = "CREATE TABLE " + TABLE_RECIPES + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_ITEM_NAME + " TEXT,"
+                + COLUMN_COOKING_TIME + " TEXT,"
+                + COLUMN_SERVINGS + " TEXT,"
+                + COLUMN_CATEGORY + " TEXT,"
+                + COLUMN_INGREDIENTS + " TEXT,"
+                + COLUMN_METHOD + " TEXT,"
+                + COLUMN_IMAGE_PATH + " TEXT"
+                + ")";
+        db.execSQL(createTableQuery);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         createTable(db,TABLE_FOOD);
         createTable(db, TABLE_HOUSEHOLD);
         createTable(db, TABLE_CLEANING);
         createTable(db, TABLE_HYGIENE);
+        createRecipesTable(db);
     }
 
     @Override
